9223882ea82f16aa47c72a4805f97818f26bccd5
David Choi <dharric@live.com>
2023-08-07 16:12:42 -0400

    fix: remvoe quotes

--- webclient/src/presentation/pages/Home.tsx
+++ webclient/src/presentation/pages/Home.tsx
@@ -9,14 +9,14 @@ export default function Home() {
       <div className="panel-col home-item-dev">
         <Link to="developer" className="home-item-link">
           <img className="home-icon" src={developer} />
-          <div className="title-font home-item-content">"I'm a Developer"</div>
+          <div className="title-font home-item-content">I'm a Developer</div>
         </Link>
       </div>
 
       <div className="panel-col home-item-emp">
         <Link to="employer" className="home-item-link">
           <img className="home-icon" src={employer} />
-          <div className="title-font home-item-content">"I'm an Employer"</div>
+          <div className="title-font home-item-content">I'm an Employer</div>
         </Link>
       </div>
     </div>
