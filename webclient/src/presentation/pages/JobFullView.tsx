2fd9f57bbbc941ea40a6e93e7256ea11ca6d35d1
David Choi <dharric@live.com>
2023-08-19 15:27:37 -0400

    chore: some style and related updates

--- webclient/src/presentation/pages/JobFullView.tsx
+++ webclient/src/presentation/pages/JobFullView.tsx
@@ -18,38 +18,68 @@ export default function JobFullView() {
   return (
     <Layout>
       <div className="dev-main" style={{ width: "738px", margin: "auto" }}>
-        <div className="dev-top">
-          <div className="title-font">{jobPost?.title}</div>
-          <div className="job-full-view-subtitle">
-            <div className="sub-title-font job-full-view-subtitle-item-primary">
-              {jobPost?.companyName}
+        <div
+          className="opposites"
+          style={{ paddingTop: "2em", paddingLeft: "2em", paddingRight: "2em" }}
+        >
+          <div className="dev-top">
+            <div className="opposites">
+              <div className="title-font">{jobPost?.title}</div>
             </div>
-            <div className="job-full-view-subtitle-item-primary">
-              {jobPost?.isRemote ? "Remote" : jobPost?.countryName}
+
+            <div className="opposites">
+              <div className="job-full-view-subtitle">
+                <div className="sub-title-font job-full-view-subtitle-item-primary">
+                  {jobPost?.companyName}
+                </div>
+                <div className="sub-title-font job-full-view-subtitle-item-primary">
+                  {jobPost?.isRemote ? "Remote" : jobPost?.countryName}
+                </div>
+                <div className="sub-title-font job-full-view-subtitle-item-primary">
+                  {jobPost?.updatedAt}
+                </div>
+              </div>
             </div>
-            <div className="job-full-view-subtitle-item-primary">
-              {jobPost?.updatedAt}
-            </div>
-          </div>
-          <div className="job-full-view-subtitle-item-secondary">
-            {`Contact: ${jobPost?.employerName}`}
-          </div>
-          <div className="job-full-view-subtitle-item-secondary">
-            {`Primary Language: ${jobPost?.primaryLangName}`}
-          </div>
-          {jobPost?.secondaryLangName ? (
+
             <div className="job-full-view-subtitle-item-secondary">
-              {`Secondary Language: ${jobPost?.secondaryLangName}`}
+              {`Contact: ${jobPost?.employerName}`}
             </div>
-          ) : null}
-          <div className="job-full-view-subtitle-item-secondary">
-            {`Industry: ${jobPost?.industryName}`}
+            <div className="job-full-view-subtitle-item-secondary">
+              {`Primary Language: ${jobPost?.primaryLangName}`}
+            </div>
+            {jobPost?.secondaryLangName &&
+            jobPost?.secondaryLangName != jobPost?.primaryLangName ? (
+              <div className="job-full-view-subtitle-item-secondary">
+                {`Secondary Language: ${jobPost?.secondaryLangName}`}
+              </div>
+            ) : null}
+            <div className="job-full-view-subtitle-item-secondary">
+              {`Industry: ${jobPost?.industryName}`}
+            </div>
+            <div className="job-full-view-subtitle-item-secondary">
+              {`Base Salary: ${salary}`}
+            </div>
           </div>
-          <div className="job-full-view-subtitle-item-secondary">
-            {`Contact: ${salary}`}
+          <div className="stack">
+            <button
+              className="primary-btn small-btn"
+              style={{ marginBottom: ".5em" }}
+            >
+              apply
+            </button>
+            <button className="secondary-btn small-btn">save</button>
           </div>
         </div>
-        <div className="dev-post-preview-container">{jobPost?.description}</div>
+
+        <div
+          className="normal-font dev-post-preview-container"
+          style={{ paddingTop: "2em", paddingLeft: "2em", paddingRight: "2em" }}
+        >
+          <span className="title-font" style={{ marginBottom: "1em" }}>
+            Description
+          </span>
+          <span style={{ paddingBottom: "2em" }}>{jobPost?.description}</span>
+        </div>
       </div>
     </Layout>
   );
